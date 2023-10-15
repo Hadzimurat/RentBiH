bf417d390327903a52af1029e2727197bce87101
Ahmed <ahmedhadzimuratovic@hotmail.com>
2023-10-15 20:25:12 +0200

    odvojena validacija za forme

--- JS/registracija.js
+++ JS/registracija.js
@@ -1,11 +1,11 @@
-function validate() {
+function validate(forma) {
 
-    var elementi = document.getElementsByClassName("unos");
+    var forma = document.getElementById(forma);
+    var elementi = forma.getElementsByClassName("unos");
 
     for (var i in elementi) {
         if (elementi[i].value === "") {
             elementi[i].classList.add("error");
-        
         }
         else{
             elementi[i].classList.remove("error");
